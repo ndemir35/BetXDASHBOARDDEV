4346bfba87351e27904e33880da6981eea7b7741
berkogrande <berk.ozturk.pt@outlook.com>
2024-01-15 03:27:35 +0300

    betx51 - fix menu translation handling

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-
 import { RouterModule } from '@angular/router';
 import { SHARED_MODULES } from '@betx/shared';
 import { INavData, SidebarModule } from '@coreui/angular-pro';
-import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 import { NgScrollbarModule } from 'ngx-scrollbar';
+import { Subscription, first } from 'rxjs';
 import { DefaultFooterComponent } from './default-footer/default-footer.component';
 import { DefaultHeaderComponent } from './default-header/default-header.component';
-import { Subscription, first } from 'rxjs';
+import { IconSubset } from '@betx/icons/icon-subset';
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'betx-default-layout',
   templateUrl: './default-layout.component.html',
   standalone: true,
   imports: [
@@ -21,50 +21,43 @@ import { Subscription, first } from 'rxjs';
     RouterModule,
     DefaultFooterComponent,
   ],
-  // providers: [TranslatePipe, TranslateService],
   styleUrls: ['./default-layout.component.scss'],
 })
 export class DefaultLayoutComponent implements OnInit, OnDestroy {
   private _subscription = new Subscription();
   navItems: INavData[] = [];
 
-  constructor(private _translateService: TranslateService) {
-    // this.navItems = [
-    //   {
-    //     name: this._translateService.instant('LOGIN.LOGIN_FORM.TITLE'),
-    //     url: '/dashboard',
-    //     iconComponent: { name: 'cil-speedometer' },
-    //   },
-    //   {
-    //     name: 'LOGIN.LOGIN_FORM.TITLE',
-    //     url: '/identity',
-    //     iconComponent: { name: 'cil-speedometer' },
-    //   },
-    // ];
-  }
+  constructor(private _translateService: TranslateService) {}
   ngOnDestroy(): void {
     this._subscription.unsubscribe();
   }
 
   ngOnInit(): void {
+    this._initMenuItems();
+  }
+
+  private _initMenuItems() {
     this._subscription.add(
-      this._translateService.onLangChange.pipe(first()).subscribe((event) => {
-        this.navItems = [
-          {
-            name: 'DASHBOARD.MENU.DASHBOARD',
-            url: '/dashboard',
-            iconComponent: { name: 'cil-speedometer' },
-          },
-          {
-            name: 'DASHBOARD.MENU.USERS',
-            url: '/user',
-            iconComponent: { name: 'cil-user' },
-          },
-        ].map((item) => ({
-          ...item,
-          name: this._translateService.instant(item.name),
-        }));
-      })
+      this._translateService
+        .get(['DASHBOARD.MENU.DASHBOARD', 'DASHBOARD.MENU.USERS'])
+        .pipe(first())
+        .subscribe((translations) => {
+          this.navItems = [
+            {
+              name: translations['DASHBOARD.MENU.DASHBOARD'],
+              url: '/dashboard',
+              iconComponent: { name: IconSubset.cilSpeedometer },
+            },
+            {
+              name: translations['DASHBOARD.MENU.USERS'],
+              url: '/user',
+              iconComponent: { name: IconSubset.cilUser },
+            },
+          ].map((item) => ({
+            ...item,
+            name: this._translateService.instant(item.name),
+          }));
+        })
     );
   }
 }
